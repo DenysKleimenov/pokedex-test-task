5160c5306fb87aa1a454b113498f99aaf0d9ce32
Denys Kleimenov <denyskleimenovit@gmail.com>
2023-03-04 16:53:23 +0200

    logo imported directly from file due to problems of appearing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Box, Container } from '@mui/material';
 
 import { PokemonsList } from './components/PokemonsList';
+import PokedexLogo from './images/pokedex.png';
 
 const App: React.FC = () => (
   <>
@@ -25,7 +26,11 @@ const App: React.FC = () => (
           textShadow: '-1px 3px 9px rgba(0, 117, 190, 0.4)',
         }}
       >
-        <img src="../images/pokedex.png" alt="" style={{ height: '70px' }} />
+        <img
+          src={PokedexLogo}
+          alt="Pokedex"
+          style={{ height: '70px' }}
+        />
       </Box>
     </Container>
     <PokemonsList />
